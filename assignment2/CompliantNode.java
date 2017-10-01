f7191815de64966d2d9b1b10e1d854c46bbea097
yokiii <yokiii.study@gmail.com>
2017-10-01 21:11:35 +0800

    Update CompliantNode.java

--- assignment2/CompliantNode.java
+++ assignment2/CompliantNode.java
@@ -3,17 +3,11 @@ import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
-    private Set<Transaction> proposal; //final proposal
-    private Set<Transaction> pending; // waiting to be sent
+    private Set<Transaction> proposal;
     private boolean[] followees;
-
-    private int rounds;
-    private int times;
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
         // IMPLEMENT THIS
-        this.rounds = numRounds;
-        this.times = 0;
     }
 
     public void setFollowees(boolean[] followees) {
@@ -24,38 +18,32 @@ public class CompliantNode implements Node {
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
         // IMPLEMENT THIS
         this.proposal = pendingTransactions;
-        this.pending = pendingTransactions;
     }
 
     public Set<Transaction> sendToFollowers() {
         // IMPLEMENT THIS
-        
-        // at k round, txs in pending list are from a node that has a path to the current node
-        // whose length is k.
-        if(times ++ < rounds)
-            return pending;
-
         return proposal;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
         // IMPLEMENT THIS
-        pending.clear();
-
+    	boolean[] sent = new boolean[followees.length];
+    	for(int i = 0; i < sent.length; i ++) {
+    		sent[i] = false;
+    	}
+    	
         Iterator<Candidate> it = candidates.iterator();
         while(it.hasNext()){
-            Candidate c = it.next();
-
-            Transaction tx = c.tx;
-            int sender = c.sender;
-
-            // in case of malicious node
-            if(followees[sender]){
-                if(proposal.contains(tx))
-                    pending.add(tx);
-                else
-                    proposal.add(tx);
-            }
+        	Candidate c = it.next();
+        	
+        	sent[c.sender] = true;
+        	if(followees[c.sender])
+        		proposal.add(c.tx);
         }
+        
+        // stop following the nodes that don't send anything at all
+    	for(int i = 0; i < sent.length; i ++) {
+    		followees[i] &= sent[i];
+    	}
     }
 }
